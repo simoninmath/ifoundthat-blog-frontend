408823abe545c7f47e44f8b8fb1b3db382f932f1
SIMONIN Mathieu <simonin.math@gmail.com>
2023-12-29 10:25:10 +0100

    [arch]-create-navigation-module-and-nav-search-components

---
+++ src/app/navigation/navigation.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
+
+
+
+@NgModule({
+  declarations: [
+    NavbarComponent,
+    SearchbarComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class NavigationModule { }
