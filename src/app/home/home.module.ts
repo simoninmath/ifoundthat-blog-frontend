8e92b589b81fc6a24e1c409f9d48596c52234342
SIMONIN Mathieu <simonin.math@gmail.com>
2023-12-29 10:10:38 +0100

    [arch]-create-news-component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HeroComponent } from './hero/hero.component';
+import { NewsComponent } from './news/news.component';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    HeroComponent
+    HeroComponent,
+    NewsComponent
   ],
   imports: [
     CommonModule
