0ececc1d3dea4c65456c4fc1d7b3557242b5b291
SIMONIN Mathieu <simonin.math@gmail.com>
2023-12-29 11:47:24 +0100

    [routes]-update-routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { Error404Component } from './error404/error404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', title: 'IFT-Blog', redirectTo: 'home', pathMatch: 'full' },   // Default Route with pathMath to avoid "side effects"
+  { path:'login', title: 'IFT-Blog', component: LoginComponent },
+  { path:'**', title: '404 error', component: Error404Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
