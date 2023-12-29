a3de360ed303f37a9a50ec0fe963b6c4e535bccc
SIMONIN Mathieu <simonin.math@gmail.com>
2023-12-29 10:28:59 +0100

    [arch]-create-footer-module-and-components

---
+++ src/app/footer/footer.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from './footer.component';
+import { CookiesComponent } from './cookies/cookies.component';
+import { LegalComponent } from './legal/legal.component';
+import { PolicyComponent } from './policy/policy.component';
+import { TermsComponent } from './terms/terms.component';
+
+
+
+@NgModule({
+  declarations: [
+    FooterComponent,
+    CookiesComponent,
+    LegalComponent,
+    PolicyComponent,
+    TermsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class FooterModule { }
