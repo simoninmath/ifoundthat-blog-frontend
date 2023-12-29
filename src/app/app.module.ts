047db05bcc230c8ec2e590505ac67af79ef28f48
SIMONIN Mathieu <simonin.math@gmail.com>
2023-12-29 10:31:31 +0100

    [arch]-create-error404-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Error404Component } from './error404/error404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
