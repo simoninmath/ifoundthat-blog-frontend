adc36619b60d1cadd2778c97f49376bbe134f034
SIMONIN Mathieu <simonin.math@gmail.com>
2023-12-29 10:55:51 +0100

    [serv]-create-newsletter-service

---
+++ src/app/service/newsletter.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsletterService {
+  private apiUrl = 'http://localhost:8000/newsletter'; // URL de votre API backend
+
+  constructor(private http: HttpClient) { }
+
+  getSubscribers(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/subscribers`);
+  }
+}
+
