da6042bd82407cb6afbf7c795c1fc6f1423e4769
SIMONIN Mathieu <simonin.math@gmail.com>
2023-12-30 11:10:59 +0100

    [comp]-list-article-component

--- src/app/home/list-article/list-article.component.ts
+++ src/app/home/list-article/list-article.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Article } from 'src/app/model/article';
+import { ArticleService } from 'src/app/service/article.service';
 
 @Component({
   selector: 'app-list-article',
   templateUrl: './list-article.component.html',
   styleUrls: ['./list-article.component.scss']
 })
+
 export class ListArticleComponent {
+
+  articleList: Article[];  // Type as a Table of Article
+
+  constructor(
+    private router: Router,
+    private articleService: ArticleService
+    ) {}
+    
+  goToArticle(article: Article) {   // Go back Method
+    this.router.navigate(['/article', article.id]);
+  };
+
+  ngOnInit() {
+    this.articleService.getArticleList()  // Get an Observable from Service
+    .subscribe(articleList => this.articleList = articleList);  // Fallow this Observable to get the article list and push it on the Component property 
+  }
 
 }
