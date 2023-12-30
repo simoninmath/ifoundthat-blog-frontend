eecaed94a7c38d8371962c0df4798e64b17160d5
SIMONIN Mathieu <simonin.math@gmail.com>
2023-12-30 10:34:21 +0100

    [model]-update-article-model

--- src/app/model/article.ts
+++ src/app/model/article.ts
@@ -1,11 +1,40 @@
-import { Category } from "./category";
 export class Article {
     id! :number;
     title! :string;
+    chapo! :string;
     content! :string;
     picture! :string;
+    category! :string[];
+    createdAt! :Date;
+    updatedAt! :Date;
     slug! :string;
-   /*  isPublished! :string;
-    updatedAt! :string;*/
-    category! : string;
+  //  isPublished! :string;
+
+  // Define default value for article Object 
+  constructor(
+    title: string = '',
+    chapo: string = '',
+    content: string = '',
+    picture: string = '',
+    category: string[] = [
+        'Cat 1',
+        'Cat 2',
+        'Cat 3',
+        'Cat 4',
+        'Cat 5',
+    ],
+    createdAt: Date = new Date,
+    updatedAt: Date = new Date,
+    slug :string = ''
+    ){
+    
+    this.title = title;
+    this.chapo = chapo;
+    this.content = content;
+    this.picture = picture;
+    this.category = category;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+    this.slug = slug;
+  }
 }
