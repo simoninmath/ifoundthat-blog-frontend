05400f02912c863a69babf194c30154b75ebc15b
SIMONIN Mathieu <simonin.math@gmail.com>
2023-12-30 10:48:29 +0100

    [model]-update-category-model

--- src/app/model/category.ts
+++ src/app/model/category.ts
@@ -2,4 +2,13 @@ export class Category {
     id! :number;
     name! :string;
     label! :string;
+
+    constructor(
+      name: string = '', 
+      label: string = '', 
+    ){
+      this.name = name;
+      this.label = label;
+    }
+
   }
