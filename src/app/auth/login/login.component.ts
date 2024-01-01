5b4abb90afe9f817595beae198235ab70d785678
SIMONIN Mathieu <simonin.math@gmail.com>
2024-01-01 21:14:25 +0100

    [comp]-create-login-component

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
 
+export class LoginComponent implements OnInit {
+  message: string = 'Status: Disconnect';
+  name: string;
+  password: string;
+  auth: AuthService;
+  
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ){}
+  
+  ngOnInit() {
+    this.auth = this.authService;
+  }
+  
+  setMessage() {
+    if(this.auth.isLoggedIn){
+      this.message = 'Connexion status: connected';
+    } else {
+      this.message = 'Wrong id or password!';
+    }
+  }
+  
+  login() {
+    this.message = 'Connexion ongoing...';
+    this.auth.logIn(this.name, this.password)
+      .subscribe((isLoggedIn: boolean) => {
+        this.setMessage();
+        if(isLoggedIn){
+          this.router.navigate(['/figurine']);
+        } else {
+          this.password = '';
+          this.router.navigate(['/login']);
+        }
+      })
+  }
+  
+  logout() {
+    this.auth.logOut();
+    this.message = 'Disconnected';
+  }  
 }
