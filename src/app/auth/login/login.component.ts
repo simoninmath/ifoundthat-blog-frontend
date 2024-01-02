26c772245d28585f40e61ada6762ee1ae7e96af8
SIMONIN Mathieu <simonin.math@gmail.com>
2024-01-02 18:23:10 +0100

    [comp]-create-login-component

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { UiService } from 'src/app/service/ui.service';
 
 @Component({
   selector: 'app-login',
@@ -9,44 +11,103 @@ import { Router } from '@angular/router';
 })
 
 export class LoginComponent implements OnInit {
-  message: string = 'Status: Disconnect';
-  name: string;
-  password: string;
-  auth: AuthService;
+  errorMessage: string;
+  form: FormGroup;
+
+  // form = new FormGroup({
+  //   email: new FormControl('', [Validators.required, Validators.email]),
+  //   password: new FormControl('', Validators.required)
+  // });
+
+  // constructor(
+  //   private auth: AuthService,
+  //   private router: Router,
+  //   private ui: UiService
+  // ) {}
   
   constructor(
-    private authService: AuthService,
-    private router: Router
-  ){}
-  
-  ngOnInit() {
-    this.auth = this.authService;
+    private formBuilder: FormBuilder,
+    private auth: AuthService,
+    private router: Router,
+    private ui: UiService
+  ) {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
   }
-  
-  setMessage() {
-    if(this.auth.isLoggedIn){
-      this.message = 'Connexion status: connected';
-    } else {
-      this.message = 'Wrong id or password!';
+
+  ngOnInit() {}
+
+  handleSubmit() {
+    if (this.form.invalid || !this.form.value.email || !this.form.value.password) {
+      return;
     }
-  }
-  
-  login() {
-    this.message = 'Connexion ongoing...';
-    this.auth.logIn(this.name, this.password)
-      .subscribe((isLoggedIn: boolean) => {
-        this.setMessage();
-        if(isLoggedIn){
-          this.router.navigate(['/figurine']);
-        } else {
-          this.password = '';
-          this.router.navigate(['/login']);
+
+    // Active l'écran de chargement
+    this.ui.activateLoading();
+
+    this.auth.authenticate(this.form.value).subscribe(
+      resultat => {
+        // Désactive l'écran de chargement
+        this.ui.deactivateLoading();
+        this.errorMessage = '';
+        this.router.navigateByUrl('/articles');
+      },
+      error => {
+        // Désactive l'écran de chargement
+        this.ui.deactivateLoading();
+
+        if (error.status === 401) {
+          this.errorMessage =
+            'Nous n\'avons pas trouvé de compte utilisateur qui corresponde avec cet email et ce mot de passe';
+
+          return;
         }
-      })
+
+        this.errorMessage =
+          'Un problème est survenu, veuillez ré-essayer plus tard';
+      }
+    );
   }
+  // message: string = 'Status: Disconnect';
+  // name: string;
+  // password: string;
+  // auth: AuthService;
   
-  logout() {
-    this.auth.logOut();
-    this.message = 'Disconnected';
-  }  
+  // constructor(
+  //   private authService: AuthService,
+  //   private router: Router
+  // ){}
+  
+  // ngOnInit() {
+  //   this.auth = this.authService;
+  // }
+  
+  // setMessage() {
+  //   if(this.auth.isLoggedIn){
+  //     this.message = 'Connexion status: connected';
+  //   } else {
+  //     this.message = 'Wrong id or password!';
+  //   }
+  // }
+  
+  // login() {
+  //   this.message = 'Connexion ongoing...';
+  //   this.auth.logIn(this.name, this.password)
+  //     .subscribe((isLoggedIn: boolean) => {
+  //       this.setMessage();
+  //       if(isLoggedIn){
+  //         this.router.navigate(['/figurine']);
+  //       } else {
+  //         this.password = '';
+  //         this.router.navigate(['/login']);
+  //       }
+  //     })
+  // }
+  
+  // logout() {
+  //   this.auth.logOut();
+  //   this.message = 'Disconnected';
+  // }  
 }
