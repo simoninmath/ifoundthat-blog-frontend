d446206b7bb99a3e5df2c751d73682eba8a25147
SIMONIN Mathieu <simonin.math@gmail.com>
2024-01-02 22:10:18 +0100

    [mock]-create-mock-article

---
+++ src/app/home/mock-article-list.ts
@@ -0,0 +1,15 @@
+import { Article } from '../model/article';
+  
+export const ARTICLES: Article[] = [
+    {
+        id: 1,
+        title: 'Article Title',
+        chapo: 'Incididunt est nulla enim adipisicing excepteur commodo cillum.Velit amet ut dolore ut consectetur esse tempor laboris minim eiusmod cillum irure occaecat.',
+        content: 'Dolore adipisicing irure incididunt id exercitation adipisicing ipsum ex dolore deserunt qui minim nulla nulla. Excepteur tempor ullamco esse et fugiat ut nostrud sunt in officia ullamco pariatur laboris laboris. Elit aliqua esse labore culpa irure. Tempor incididunt et Lorem velit aute ea. Aliquip ea magna irure proident pariatur tempor ex dolor. Ipsum consectetur quis exercitation velit Lorem. Est reprehenderit excepteur aute velit. Consectetur nisi ut occaecat deserunt. Eu mollit labore occaecat enim tempor proident est veniam velit officia sunt exercitation id sint. Labore elit tempor aliquip fugiat voluptate quis veniam sint. Aliqua irure anim consequat cillum cillum cillum dolore adipisicing deserunt nisi sit adipisicing sint ea. Nostrud nisi nisi eu mollit id sint labore ut commodo eu ut. Fugiat esse ut ullamco dolore veniam quis culpa est qui eiusmod ad irure duis duis. Est voluptate dolor fugiat dolore esse quis deserunt occaecat consectetur incididunt enim culpa. Dolore id proident officia sit elit nisi ut. Eiusmod adipisicing culpa adipisicing voluptate exercitation duis minim occaecat. Laborum eiusmod est minim sunt cupidatat reprehenderit officia sit ut officia. Ullamco aliquip esse amet voluptate eu Lorem ut enim dolor nostrud duis eu veniam nulla. Pariatur ut sit commodo excepteur ullamco. Cillum occaecat mollit dolor sunt dolore laboris voluptate ipsum qui voluptate esse proident occaecat labore. Adipisicing enim culpa eu dolore ad velit tempor anim.',
+        picture: '',
+        category: ['Cat 1'],
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        slug: ''
+    },
+];
