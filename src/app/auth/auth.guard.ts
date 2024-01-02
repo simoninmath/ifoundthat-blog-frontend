d79d99978de3fe8ba58b5c4eb9ac1ad1eb5ee702
SIMONIN Mathieu <simonin.math@gmail.com>
2024-01-02 18:19:44 +0100

    [guard]-update-guard

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -14,4 +14,20 @@ export const AuthGuard = () => {
 
   router.navigate(['/login']);
   return false;
+
+  // canActivate(
+  //   next: ActivatedRouteSnapshot,
+  //   state: RouterStateSnapshot
+  // ):
+  //   | Observable<boolean | UrlTree>
+  //   | Promise<boolean | UrlTree>
+  //   | boolean
+  //   | UrlTree {
+  //   if (this.auth.isAuthenticated()) {
+  //     return true;
+  //   }
+  //   this.router.navigateByUrl('/login');
+  //   return false;
+  // }
+
 }
