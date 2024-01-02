21141314ee910de5d5dc5ae6509f9fefdc27b895
SIMONIN Mathieu <simonin.math@gmail.com>
2024-01-02 22:00:16 +0100

    [pipe]-create-pipe-category-article-color

---
+++ src/app/home/category-article.pipe.ts
@@ -0,0 +1,35 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'categoryTypeColor'
+})
+
+export class CategoryTypeColorPipe implements PipeTransform {
+
+  transform(category: string): string {
+    let color: string = 'grey lighten-1';  // Default color value
+
+    switch (category) {
+      case 'Default':
+        color = 'red lighten-1';
+        break;
+      case 'Cat 1':
+        color = 'blue lighten-1';
+        break;
+      case 'Cat 2':
+        color = 'green lighten-2';
+        break;
+      case 'Cat 3':
+        color = 'brown lighten-3';
+        break;
+      case 'Cat 4':
+        color = 'grey lighten-1';
+        break;
+      case 'Cat 5':
+        color = 'blue lighten-3';
+        break;
+    }
+
+    return 'new badge ' + color;
+  }
+}
