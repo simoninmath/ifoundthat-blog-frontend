85831cc39514c50994af00e0a100ae3098ab4cea
SIMONIN Mathieu <simonin.math@gmail.com>
2024-01-02 18:22:27 +0100

    [module]-update-auth-module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,16 +2,38 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { RouterModule, Routes } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './auth.interceptor';
 
-
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }
+];
 
 @NgModule({
   declarations: [
-    RegisterComponent,
-    LoginComponent
+    RegisterComponent
   ],
   imports: [
     CommonModule,
+    SharedModule,
+    RouterModule.forChild(routes)],
+  exports: [
+    RouterModule
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    },
+    {
+      provide: Storage,
+      useValue: window.localStorage
+    }
   ]
 })
+
 export class AuthModule { }
