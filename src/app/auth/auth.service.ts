d92a90901cf72593cb77c6088e542b686e130488
SIMONIN Mathieu <simonin.math@gmail.com>
2024-01-04 15:29:37 +0100

    [refact]-debug-auth-service-with-token-type-not-null

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,10 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import jwtDecode from 'jwt-decode';
+// import * as jwtDecode from 'jwt-decode';
 import { Observable, Subject, of } from 'rxjs';
-import { delay, map, tap } from 'rxjs/operators';
+import { catchError, delay, map, tap } from 'rxjs/operators';
 import { environment } from '../../environment/environment';
 import { Credentials } from './credentials';
+import { jwtDecode } from 'jwt-decode';
 
 // URL from
 const USERS_API = environment.USERS_API;
@@ -16,28 +17,24 @@ interface AuthResponse {
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class AuthService {
   isLoggedIn: boolean = false;
   redirectUrl: string;
   authState = new Subject<boolean>();
 
-  constructor(
-    private http: HttpClient, 
-    private storage: Storage
-  ) {}
+  constructor(private http: HttpClient, private storage: Storage) {}
 
   // Login is a default asynchronous operation, so we need to use Observable
   logIn(name: string, password: string): Observable<boolean> {
-    const isLoggedIn = (name == 'fig' && password == 'fig');
-  
-  // Simulate server answer with delay
-  return of(isLoggedIn).pipe(
-    delay(1000),
-    tap(isLoggedIn => this.isLoggedIn = isLoggedIn)
-   );
+    const isLoggedIn = name == 'fig' && password == 'fig';
+
+    // Simulate server answer with delay
+    return of(isLoggedIn).pipe(
+      delay(1000),
+      tap((isLoggedIn) => (this.isLoggedIn = isLoggedIn))
+    );
   }
 
   logOut() {
@@ -49,11 +46,14 @@ export class AuthService {
   }
 
   authenticate(credentials: Credentials) {
-    return this.http.post(AUTH_API, credentials).pipe(
+    return this.http.post<AuthResponse>(AUTH_API, credentials).pipe(
       map((result: AuthResponse) => {
         this.storage.setItem('token', result.token);
         this.authState.next(true);
         return result;
+      }),
+      catchError((error: any) => {
+        throw new Error(`Error!`);
       })
     );
   }
@@ -72,9 +72,11 @@ export class AuthService {
     return this.getToken() !== null;
   }
 
-  getUserData() {
-    if (!this.getToken()) { return null; }
-    return jwtDecode(this.getToken());
+  getUserData(): any | null {
+    const token = this.getToken();
+    if (!token) {
+      return null;
+    }
+    return jwtDecode(token);
   }
-
 }
