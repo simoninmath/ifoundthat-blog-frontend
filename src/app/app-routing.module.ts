842eaef13adcf95ca1c46301bf8c6eadebc55cb4
SIMONIN Mathieu <simonin.math@gmail.com>
2024-01-04 22:01:49 +0100

    [route]-add-register-to-routing-module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { Error404Component } from './error404/error404.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 const routes: Routes = [
   { path:'', title: 'IFT-Blog', redirectTo: 'home', pathMatch: 'full' },   // Default Route with pathMath to avoid "side effects"
   { path:'login', title: 'IFT-Blog', component: LoginComponent },
+  { path:'register', title: 'IFT-Blog', component: RegisterComponent },
   { path:'**', title: '404 error', component: Error404Component }
 ];
 
