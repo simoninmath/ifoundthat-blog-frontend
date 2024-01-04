26c7bbd615cf5ce7bb23b27884e7c91f56ba8ca0
SIMONIN Mathieu <simonin.math@gmail.com>
2024-01-04 18:07:30 +0100

    [comp]-update-register-component-with-getters-in-interpolations-fields

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -11,7 +11,6 @@ import { Violation } from '../violation';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
-
 export class RegisterComponent implements OnInit {
   error = false;
   form: FormGroup;
@@ -35,13 +34,7 @@ export class RegisterComponent implements OnInit {
   handleSubmit() {
     this.ui.activateLoading();
 
-    if (
-      this.form.invalid ||
-      !this.form.value.email ||
-      !this.form.value.password ||
-      !this.form.value.fullName ||
-      !this.form.value.plainPassword
-    ) {
+    if (this.form.invalid) {
       this.ui.deactivateLoading();
       return;
     }
@@ -73,3 +66,81 @@ export class RegisterComponent implements OnInit {
     );
   }
 }
+
+
+
+// import { Component, OnInit } from '@angular/core';
+// import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+// import { AuthService } from '../auth.service';
+// import { Router } from '@angular/router';
+// import { HttpErrorResponse } from '@angular/common/http';
+// import { UiService } from 'src/app/service/ui.service';
+// import { Violation } from '../violation';
+
+// @Component({
+//   selector: 'app-register',
+//   templateUrl: './register.component.html',
+//   styleUrls: ['./register.component.scss'],
+// })
+
+// export class RegisterComponent implements OnInit {
+//   error = false;
+//   form: FormGroup;
+
+//   constructor(
+//     private formBuilder: FormBuilder,
+//     private auth: AuthService,
+//     private router: Router,
+//     private ui: UiService
+//   ) {
+//     this.form = this.formBuilder.group({
+//       email: ['', [Validators.required, Validators.email]],
+//       password: ['', Validators.required],
+//       fullName: ['', Validators.required],
+//       plainPassword: ['', Validators.required],
+//     });
+//   }
+
+//   ngOnInit() {}
+
+//   handleSubmit() {
+//     this.ui.activateLoading();
+
+//     if (
+//       this.form.invalid ||
+//       !this.form.value.email ||
+//       !this.form.value.password ||
+//       !this.form.value.fullName ||
+//       !this.form.value.plainPassword
+//     ) {
+//       this.ui.deactivateLoading();
+//       return;
+//     }
+
+//     this.auth.register(this.form.value).subscribe(
+//       () => {
+//         this.ui.deactivateLoading();
+//         this.router.navigateByUrl('/login');
+//       },
+//       (httpError: HttpErrorResponse) => {
+//         this.ui.deactivateLoading();
+
+//         if (httpError.status === 400) {
+//           const violations = httpError.error.violations as Violation[];
+
+//           for (const apiViolation of violations) {
+//             const control = this.form.get(apiViolation.propertyPath);
+//             if (control) {
+//               control.setErrors({
+//                 violation: apiViolation.message,
+//               });
+//             }
+//           }
+//           return;
+//         }
+//         // Else
+//         this.error = true;
+//       }
+//     );
+//   }
+// }
