f732a8c82e070baf83da5298005d2a1790b3ff9a
SIMONIN Mathieu <simonin.math@gmail.com>
2024-01-21 11:17:40 +0100

    [style]-news-section-styles

--- src/app/home/news/news.component.ts
+++ src/app/home/news/news.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+// import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
   styleUrls: ['./news.component.scss']
 })
-export class NewsComponent {
+
+export class NewsComponent implements OnInit {
+  
+  imgNewsMain: string;
+
+  // constructor(
+  //   private router: Router
+  // ){}
+
+  ngOnInit() {
+    this.imgNewsMain = 'https://images.unsplash.com/photo-1517857612127-f33b2b246bd6?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+  }
+
+  goToReddit() {
+    const redditWebSite = 'https://www.reddit.com/';
+    window.open(redditWebSite, '_blank', 'noopener,noreferrer');  // Open the link in a new window, and avoid the reference call back to secure the external link
+  }
 
 }
