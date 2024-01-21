a337408b63ffef1593dba931db337e5ee49bff84
SIMONIN Mathieu <simonin.math@gmail.com>
2024-01-21 17:50:40 +0100

    [clean]-news-component-logic-delete-constructor

--- src/app/home/news/news.component.ts
+++ src/app/home/news/news.component.ts
@@ -12,10 +12,6 @@ export class NewsComponent implements OnInit {
   
   imgNewsMain: string;
 
-  // constructor(
-  //   private router: Router
-  // ){}
-
   ngOnInit() {
     this.imgNewsMain = 'https://images.unsplash.com/photo-1517857612127-f33b2b246bd6?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
   }
