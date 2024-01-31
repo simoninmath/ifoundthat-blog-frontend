95d80cfb2fc345cfce1f1fc14cd57297a7d9bf3f
SIMONIN Mathieu <simonin.math@gmail.com>
2024-01-31 06:44:40 +0100

    [style]-styling-with-grid

--- src/app/navigation/searchbar/searchbar.component.ts
+++ src/app/navigation/searchbar/searchbar.component.ts
@@ -10,34 +10,34 @@ import { ArticleService } from 'src/app/service/article.service';
   styleUrls: ['./searchbar.component.scss']
 })
 
-export class SearchbarComponent implements OnInit {
-  searchTerms = new Subject<string>();  // Subject is an RxJS library Object
-  articles$: Observable<Article[]>;  // For create a flow with user's terms in a Table
+export class SearchbarComponent {
+  // searchTerms = new Subject<string>();  // Subject is an RxJS library Object
+  // articles$: Observable<Article[]>;  // For create a flow with user's terms in a Table
 
-  constructor(
-    private router: Router,
-    private articleService: ArticleService
-    ) {
-  }
+  // constructor(
+  //   private router: Router,
+  //   private articleService: ArticleService
+  //   ) {
+  // }
 
-  ngOnInit() { // Firstable, clean up the user search expression
-    // {..."a"."ab"..."abz"."ab"...."abc"......}
-    this.articles$ = this.searchTerms.pipe(
-      debounceTime(300),
-    // {......"ab"...."ab"...."abc"......}
-      distinctUntilChanged(),
-    // {......"ab"............"abc"......}
-      switchMap((term) => this.articleService.searchArticleList(term)) // Then, call the server
-    // {......articleList(ab).......articleList(abc).....}
-      );
-  }
+  // ngOnInit() { // Firstable, clean up the user search expression
+  //   // {..."a"."ab"..."abz"."ab"...."abc"......}
+  //   this.articles$ = this.searchTerms.pipe(
+  //     debounceTime(300),
+  //   // {......"ab"...."ab"...."abc"......}
+  //     distinctUntilChanged(),
+  //   // {......"ab"............"abc"......}
+  //     switchMap((term) => this.articleService.searchArticleList(term)) // Then, call the server
+  //   // {......articleList(ab).......articleList(abc).....}
+  //     );
+  // }
 
-  search(term: string) {
-    this.searchTerms.next(term); // If a user search a term, this Method push the result of Subject
-  }
+  // search(term: string) {
+  //   this.searchTerms.next(term); // If a user search a term, this Method push the result of Subject
+  // }
 
-  goToDetailArticle(article: Article) {
-    const link = ['/article', article.id]; // Construct the link where the user will be redirected according to article id
-    this.router.navigate(link); // Redirect the user with Angular Router
-  }
+  // goToDetailArticle(article: Article) {
+  //   const link = ['/article', article.id]; // Construct the link where the user will be redirected according to article id
+  //   this.router.navigate(link); // Redirect the user with Angular Router
+  // }
 }
