108a3831c9700c49be910d748e0c1ddf6f6a6c07
SIMONIN Mathieu <simonin.math@gmail.com>
2024-01-31 06:30:17 +0100

    [comment]-admin-guard

--- src/app/auth/admin.guard.ts
+++ src/app/auth/admin.guard.ts
@@ -2,17 +2,17 @@ import { inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
 
-// Refactored with Guard Function and inject(), 
-// because CanActivate class is now depreciated
 export const AdminGuard = () => {
-  const authService = inject(AuthService);
+  const authService = inject(AuthService);  // Dependency Injection using inject() method
   const router = inject(Router);
 
+  // Check if the user is authenticated as an admin using the AuthService
   if(authService.isAdmin()){
+    // If authenticated as an admin, allow access to the route
     return true;
   }
 
+  // If not authenticated as an admin, navigate to the home page and deny access
   router.navigate(['/home']);
   return false;
-
 }
