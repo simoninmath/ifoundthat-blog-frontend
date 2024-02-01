5a25fe2e52f6f90f617e2e36ce2d1cb45bc2645d
SIMONIN Mathieu <simonin.math@gmail.com>
2024-02-01 11:30:54 +0100

    [comp]-update-navigation-component-logic-and-styles

--- src/app/navigation/navbar/navbar.component.ts
+++ src/app/navigation/navbar/navbar.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable, Subject, debounceTime, distinctUntilChanged, switchMap } from 'rxjs';
 import { AuthService } from 'src/app/auth/auth.service';
+import { Article } from 'src/app/model/article';
+import { ArticleService } from 'src/app/service/article.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,7 +11,37 @@ import { AuthService } from 'src/app/auth/auth.service';
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
-  constructor(private authService: AuthService) {}
+
+  searchTerms = new Subject<string>();  // Subject is an RxJS library Object
+  articles$: Observable<Article[]>;  // For create a flow with user's terms in a Table
+
+  constructor(
+    private router: Router,
+    private articleService: ArticleService,
+    private authService: AuthService
+    ) {
+  }
+
+  ngOnInit() { // Firstable, clean up the user search expression
+    // {..."a"."ab"..."abz"."ab"...."abc"......}
+    this.articles$ = this.searchTerms.pipe(
+      debounceTime(300),
+    // {......"ab"...."ab"...."abc"......}
+      distinctUntilChanged(),
+    // {......"ab"............"abc"......}
+      switchMap((term) => this.articleService.searchArticleList(term)) // Then, call the server
+    // {......articleList(ab).......articleList(abc).....}
+      );
+  }
+
+  search(term: string) {
+    this.searchTerms.next(term); // If a user search a term, this Method push the result of Subject
+  }
+
+  goToDetailArticle(article: Article) {
+    const link = ['/article', article.id]; // Construct the link where the user will be redirected according to article id
+    this.router.navigate(link); // Redirect the user with Angular Router
+  }
 
   isLogin(): any {
     return this.authService.isAuthenticated();
@@ -20,4 +54,5 @@ export class NavbarComponent {
   getUser(){
     this.authService.getUserFromToken();
   }
+
 }
