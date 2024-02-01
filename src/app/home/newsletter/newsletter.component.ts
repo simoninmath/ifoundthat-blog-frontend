53b973be873b2cad585477d9c2d2f5c57518a38f
SIMONIN Mathieu <simonin.math@gmail.com>
2024-02-01 11:28:12 +0100

    [comp]-update-comments-into-newsletter

--- src/app/home/newsletter/newsletter.component.ts
+++ src/app/home/newsletter/newsletter.component.ts
@@ -8,13 +8,11 @@ import { tap } from 'rxjs';
 @Component({
   selector: 'app-newsletter',
   templateUrl: './newsletter.component.html',
-  styleUrls: ['./newsletter.component.scss']
+  styleUrls: ['./newsletter.component.scss'],
 })
-
 export class NewsletterComponent implements OnInit {
-
-  newsletters: Newsletter[] = [];  // Need an iterable for get collection method
-  newslettersModel: Newsletter;  // Need a data model for post method
+  newsletters: Newsletter[] = []; // Need an iterable for get collection method
+  newslettersModel: Newsletter; // Need a data model for post method
   newslettersForm: FormGroup; // Need form group class to form secured
 
   // Use dependency injection to access services
@@ -22,16 +20,14 @@ export class NewsletterComponent implements OnInit {
     private newsletterService: NewsletterService,
     private router: Router,
     private formBuilder: FormBuilder
-    ) {}
-  
-    // Initialize validators for each field
-    private initForm() {
-      this.newslettersForm = this.formBuilder.group({
-        email: ['', [Validators.required, Validators.email]],
-        // createdAt: ['', Validators.required],
-      });
-    }
+  ) {}
 
+  // Initialize validators for each field
+  private initForm() {
+    this.newslettersForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
 
   // The method below launches methods when the program starts
   ngOnInit() {
@@ -39,26 +35,28 @@ export class NewsletterComponent implements OnInit {
     this.initForm();
   }
 
-
   // This method call newsletter service to get newsletter user email object
   getUserEmailFromNewsletter() {
-    this.newsletterService.getUserEmailFromNewsletter()
-    .subscribe((response: Newsletter[]) => {  // This method subscribes to the Observable of newsletter.service.ts
-      this.newsletters = response;   
-    });
+    this.newsletterService
+      .getUserEmailFromNewsletter()
+      .subscribe((response: Newsletter[]) => {
+        // This method subscribes to the Observable of newsletter.service.ts
+        this.newsletters = response;
+      });
   }
-
 
   onSubmitNewsletters() {
     if (this.newslettersForm.valid) {
       const createDataNewsletter: Newsletter = {
         email: this.newslettersForm.value.email,
       };
-  
-      this.newsletterService.createFormNewsletter(createDataNewsletter).pipe(
-        tap(() => this.router.navigate(['/home'])),  // Updated navigation
-      ).subscribe();
+
+      this.newsletterService
+        .createFormNewsletter(createDataNewsletter)
+        .pipe(
+          tap(() => this.router.navigate(['/home'])) // Updated navigation
+        )
+        .subscribe();
     }
   }
-
 }
